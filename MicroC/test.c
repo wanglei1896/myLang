7ddfe18bd769cb44ac87c284d4f52e9a2a07610a
you@example.com <Your Name>
2017-07-03 12:29:03 +0800

    sad
    ssssddddd

---
+++ MicroC/test.c
@@ -0,0 +1,15 @@
+void main()
+{
+	char a[7];
+	a[0]='H';
+	a[1]='e';
+	a[2]='l';
+	a[3]='l';
+	a[4]='o';
+	a[5]='!';
+	int i;
+	for(i=0; i<6; ++i)
+		printc a[i];
+	//prints "Hello world!";
+	println;
+}
